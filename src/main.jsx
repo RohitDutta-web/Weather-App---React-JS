083a1d5f592b28628f60e949cc885b0b12ad9354
rohit <duttarohit754@gmail.com>
2024-08-19 18:08:34 +0530

    browser route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { StrictMode, BrowserRouter, } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename='/Weather-App---React-JS/'>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
